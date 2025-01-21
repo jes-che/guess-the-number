d1d7b12867f00953657451a61cbc3985f86234cf
jes-che <jessicachendeveloper@gmail.com>
2025-01-20 22:36:58 -0800

    Implement game logic

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 'use strict';
 
+const targetNum = Math.floor(Math.random() * 10) + 1;
+document.querySelector('.number').textContent = targetNum;
+let score = 10;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
     if (!guess) {
         document.querySelector('.message').textContent = 'Invalid guess! Enter a number!';
+    } else if (guess === targetNum) {
+        document.querySelectorAll('.before').forEach(el => {
+            el.classList.add('hidden');
+        });
+        document.querySelector('.after-win').classList.remove('hidden');
+        document.querySelector('#score').append(`Your Score: ${score}`);
+        document.querySelectorAll('.restart').forEach(el => {
+            el.addEventListener('click', function () {
+                window.location.reload();
+                return false;
+            })
+        });
+    } else if (guess !== targetNum) {
+        if (score > 1) {
+            if (guess > targetNum) {
+                document.querySelector('.message').textContent = 'Too high! Guess again!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else if (guess < targetNum) {
+                document.querySelector('.message').textContent = 'Too low! Guess again!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+        } else {
+            document.querySelectorAll('.before').forEach(el => {
+                el.classList.add('hidden');
+            })
+            document.querySelector('.after-loss').classList.remove('hidden');
+            document.querySelectorAll('.restart').forEach(el => {
+                el.addEventListener('click', function () {
+                    window.location.reload();
+                    return false;
+                })
+            });
+        }
     }
 });
